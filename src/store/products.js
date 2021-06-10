9bc8883bbcb9ac05cee487230de1b44c143f9289
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-06-09 17:30:30 -0700

    api url typo fix

--- src/store/products.js
+++ src/store/products.js
@@ -2,7 +2,7 @@ import superagent from "superagent";
 
 let list = [];
 
-let api = "https://ellis-api-server.herokuapp.com//product";
+let api = "https://ellis-api-server.herokuapp.com/product";
 
 export default (state = list, action) => {
   let { type, payload } = action;
